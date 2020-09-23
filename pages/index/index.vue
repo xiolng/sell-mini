eb582394436edbe3507c3c438edb41958646bec1
xiolng <zheng_zhanning@126.com>
2020-09-23 10:46:09 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,61 @@
+<template>
+	<view class="container">
+		<view class="qr-box">
+			<view class="qrimg">
+				<tki-qrcode
+					ref="qrcode"
+					cid="qrcode"
+					:val="href"
+					size="250"
+					:unit="250"
+					@result="qrR"
+				/>
+			</view>
+		</view>
+		<view class="btn-box">
+			<u-button
+				type='primary'
+				@click='$refs.qrcode._saveCode()'
+			>
+				保存二维码
+			</u-button>
+		</view>
+	</view>
+</template>
+
+<script>
+import tkiQrcode from 'tki-qrcode';
+export default {
+	data() {
+		return {
+			href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+		};
+	},
+	mounted(){
+		console.log(222,this.$refs)
+		this.$refs.qrcode._makeCode()
+	},
+	methods: {
+		qrR(res){
+			console.log(333, res)
+		}
+	},
+	components: {
+		tkiQrcode
+	}
+};
+</script>
+
+<style lang="scss">
+.container {
+	padding: 20px;
+	font-size: 14px;
+	line-height: 24px;
+	.qr-box{
+		height: 400px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+}
+</style>
---
+++ pages/my/my.vue
@@ -0,0 +1,20 @@
+<template>
+	<view class="my">
+		<u-cell-group>
+			<u-cell-item icon="account" title="个人信息" @click="$u.route({ url: '/pages/my/userInfo' })" />
+			<u-cell-item icon="search" title="业绩查询" @click="$u.route({ url: '/pages/my/performanceQuery' })" />
+			<u-cell-item icon="order" title="兑现记录" @click="$u.route({ url: '/pages/my/cashRecords' })" />
+			<u-cell-item icon="list-dot" title="业绩排名" @click="$u.route({ url: '/pages/my/ranking' })" />
+		</u-cell-group>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss"></style>
